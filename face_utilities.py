91f8006c05f341d4291598b0f54c7dd6dc2aa517
Kingshook <150927041+Tasiyaah@users.noreply.github.com>
2024-10-09 13:28:12 +0900

    face_utilities.py

--- face_utilities.py
+++ face_utilities.py
@@ -6,25 +6,28 @@ import imutils
 from collections import OrderedDict
 
 
-class Face_utilities:
+class Face_utilities():
     '''
-    This class contains all needed functions to work with faces in a frame'
+    This class contains all needed functions to work with faces in a frame
     '''
-
-    def __init__(self,face_width = 200):
+    
+    def __init__(self, face_width = 200):
         self.detector = None
-        self.predictor = None
-
-        self. MODEL_MEAN_VALUES = (78.4263377603, 87.7689143744, 114.895847746)
-
-        self.desiredLeftEye = (0.35,0.35)
+        
+        self.predictor = None 
+        self.age_net = None
+        self.gender_net = None
+        
+        self.MODEL_MEAN_VALUES = (78.4263377603, 87.7689143744, 114.895847746)
+        
+        
+        self.desiredLeftEye=(0.35, 0.35)
         self.desiredFaceWidth = face_width
         self.desiredFaceHeight = None
-
+        
         if self.desiredFaceHeight is None:
             self.desiredFaceHeight = self.desiredFaceWidth
-        
-        #for dlib’s 68-point facial landmark detector:
+        #For dlib’s 68-point facial landmark detector:
         self.FACIAL_LANDMARKS_68_IDXS = OrderedDict([
             ("mouth", (48, 68)),
             ("right_eyebrow", (17, 22)),
@@ -41,14 +44,18 @@ class Face_utilities:
             ("left_eye", (0, 1)),
             ("nose", (4))   
         ])
-
+        
         #last params
-        self.last_reacts = None
+        self.last_age = None
+        self.last_gender = None
+        self.last_rects = None
         self.last_shape = None
-        self.last_alligned_shape = None
+        self.last_aligned_shape = None
+        
+        #FACIAL_LANDMARKS_IDXS = FACIAL_LANDMARKS_68_IDXS
     
     def face_alignment(self, frame, shape):
-    
+       
         if (len(shape)==68):
 			# extract the left and right eye (x, y)-coordinates
             (lStart, lEnd) = self.FACIAL_LANDMARKS_68_IDXS["left_eye"]
@@ -68,14 +75,8 @@ class Face_utilities:
         dX = rightEyeCenter[0] - leftEyeCenter[0]
         angle = np.degrees(np.arctan2(dY, dX)) - 180
         
-        # compute the desired right eye x-coordinate based on the
-        # desired x-coordinate of the left eye
         desiredRightEyeX = 1.0 - self.desiredLeftEye[0]
-        
-        # determine the scale of the new resulting image by taking
-        # the ratio of the distance between eyes in the *current*
-        # image to the ratio of distance between eyes in the
-        # *desired* image
+       
         dist = np.sqrt((dX ** 2) + (dY ** 2))
         desiredDist = (desiredRightEyeX - self.desiredLeftEye[0])
         desiredDist *= self.desiredFaceWidth
@@ -104,17 +105,20 @@ class Face_utilities:
         #print(aligned_shape.shape)
         
         if(len(shape)==68):
-            shape = np.reshape(shape,(68,1,2))           
+            shape = np.reshape(shape,(68,1,2))
+        
+                    
         else:
             shape = np.reshape(shape,(5,1,2))
+          
                   
         aligned_shape = cv2.transform(shape, M)
         aligned_shape = np.squeeze(aligned_shape)        
-        
+      
         return aligned_face,aligned_shape
     
     def face_detection(self, frame):
-      
+        
         if self.detector is None:
             self.detector = dlib.get_frontal_face_detector()
         
@@ -125,10 +129,12 @@ class Face_utilities:
         #get all faces in the frame
         rects = self.detector(gray, 0)
         # to get the coords from a rect, use: (x, y, w, h) = face_utils.rect_to_bb(rects[0])
+        
         return rects
     
+    
     def get_landmarks(self, frame, type):
-        
+     
         if self.predictor is None:
             print("[INFO] load " + type + " facial landmarks model ...")
             self.predictor = dlib.shape_predictor("shape_predictor_" + type + "_face_landmarks.dat")
@@ -136,16 +142,21 @@ class Face_utilities:
         
         if frame is None:
             return None, None
+        
         gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
         rects = self.face_detection(frame)
+        
         if len(rects)<0 or len(rects)==0:
             return None, None
+            
         shape = self.predictor(gray, rects[0])
         shape = face_utils.shape_to_np(shape)
+        
+       
         return shape, rects
     
     def ROI_extraction(self, face, shape):
-      
+       
         if (len(shape)==68):
             ROI1 = face[shape[29][1]:shape[33][1], #right cheek
                     shape[54][0]:shape[12][0]]
@@ -161,8 +172,89 @@ class Face_utilities:
                     
             ROI2 =  face[int((shape[4][1] + shape[2][1])/2):shape[4][1], #left cheek
                     shape[1][0]:shape[0][0]]
-            
             ROI3 = face[int((shape[4][1] + shape[2][1]) / 2):shape[4][1], #nose
                    shape[1][0]:shape[0][0]]
+                   
+                
+        return ROI1, ROI2, ROI3      
+   
+    def facial_landmarks_remap(self,shape):
+      
+        
+        remapped_shape = shape.copy()
+        # left eye brow
+        remapped_shape[17] = shape[26]
+        remapped_shape[18] = shape[25]
+        remapped_shape[19] = shape[24]
+        remapped_shape[20] = shape[23]
+        remapped_shape[21] = shape[22]
+        # right eye brow
+        remapped_shape[22] = shape[21]
+        remapped_shape[23] = shape[20]
+        remapped_shape[24] = shape[19]
+        remapped_shape[25] = shape[18]
+        remapped_shape[26] = shape[17]
+        # neatening 
+        remapped_shape[27] = shape[0]
+        
+        remapped_shape = cv2.convexHull(shape)
+        #to use remapped_shape
+        #mask = np.zeros((face_frame.shape[0], face_frame.shape[1])) #create a black rectangle mask with w, h of the face
+        #cv2.fillConvexPoly(mask, remapped_shape[0:27], 1) #fill convex to the mask with remapped_shape
+        
+        return remapped_shape       
+    
+    def no_age_gender_face_process(self, frame, type):
+     
+        if(type=="5"):
+            shape, rects = self.get_landmarks(frame, "5")
             
-        return ROI1, ROI2 , ROI3      
+            if shape is None:
+                return None
+        else:    
+            shape, rects = self.get_landmarks(frame, "68")
+            if shape is None:
+                return None
+        
+        (x, y, w, h) = face_utils.rect_to_bb(rects[0])
+        
+        face = frame[y:y+h,x:x+w]
+        aligned_face,aligned_shape = self.face_alignment(frame, shape)
+        
+                
+        return rects, face, shape, aligned_face, aligned_shape
+        
+    def face_full_process(self, frame, type, face_detect_on, others_on):
+        #assign from last params
+        rects = self.last_rects
+        shape = self.last_shape
+        aligned_shape = self.last_aligned_shape
+        
+        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+        
+        if face_detect_on:
+            if(type=="5"):
+                shape, rects = self.get_landmarks(frame, "5")
+                #mask = None
+                
+                if shape is None:
+                    return None
+            else:    
+                shape, rects = self.get_landmarks(frame, "68")
+               
+                if shape is None:
+                    return None
+        
+        (x, y, w, h) = face_utils.rect_to_bb(rects[0])
+        
+        face = frame[y:y+h,x:x+w]        
+        
+        
+        aligned_face, aligned_face = self.face_alignment(frame, shape)
+        
+        self.last_rects = rects
+        self.last_shape = shape
+        self.last_aligned_shape = aligned_shape
+        
+        #return rects, face,  shape, aligned_face, mask
+        return rects, face,  shape, aligned_face, aligned_shape
