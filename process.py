422cbee7362dbc25d4a6ed06fa3eb5b4236c22ec
Kingshook <150927041+Tasiyaah@users.noreply.github.com>
2024-10-09 13:28:43 +0900

    process.py

--- process.py
+++ process.py
@@ -6,14 +6,15 @@ from scipy import signal
 from face_utilities import Face_utilities
 from signal_processing import Signal_processing
 from imutils import face_utils
+# from sklearn.decomposition import FastICA
 
-class Process (object):
+class Process(object):
     def __init__(self):
-        self.frame_in = np.zeros((10,10,3),np.uint8)
+        self.frame_in = np.zeros((10, 10, 3), np.uint8)
         self.frame_ROI = np.zeros((10, 10, 3), np.uint8)
         self.frame_out = np.zeros((10, 10, 3), np.uint8)
         self.samples = []
-        self.buffer_size = 150
+        self.buffer_size = 100
         self.times = [] 
         self.data_buffer = []
         self.fps = 0
@@ -27,17 +28,27 @@ class Process (object):
         self.fu = Face_utilities()
         self.sp = Signal_processing()
 
+        
+        
     def extractColor(self, frame):
+        
+       
         g = np.mean(frame)
+        
         return g
-    
+        
     def run(self):
+        # frame, face_frame, ROI1, ROI2, status, mask = self.fd.face_detect(self.frame_in)
+        
         frame = self.frame_in
         ret_process = self.fu.no_age_gender_face_process(frame, "5")
         if ret_process is None:
             return False
         rects, face, shape, aligned_face, aligned_shape = ret_process
 
+        (x, y, w, h) = face_utils.rect_to_bb(rects[0])
+        cv2.rectangle(frame,(x,y),(x+w,y+h),(255,0,0),2)
+        
         if(len(aligned_shape)==68):
             cv2.rectangle(aligned_face,(aligned_shape[54][0], aligned_shape[29][1]), #draw rectangle on right and left cheeks
                     (aligned_shape[12][0],aligned_shape[33][1]), (0,255,0), 0)
@@ -48,23 +59,29 @@ class Process (object):
         else:
             cv2.rectangle(aligned_face, (aligned_shape[0][0],int((aligned_shape[4][1] + aligned_shape[2][1])/2)),
                         (aligned_shape[1][0],aligned_shape[4][1]), (0,255,0), 0)
+            
             cv2.rectangle(aligned_face, (aligned_shape[2][0],int((aligned_shape[4][1] + aligned_shape[2][1])/2)),
                         (aligned_shape[3][0],aligned_shape[4][1]), (0,255,0), 0)
+            
             cv2.rectangle(aligned_face, (aligned_shape[2][0] - 10, aligned_shape[2][1]),  
-              (aligned_shape[2][0] + 10, aligned_shape[4][1]),  (0, 255, 0), 2)  
-    
+              (aligned_shape[2][0] + 10, aligned_shape[4][1]),  (0, 255, 0), 2) 
+        
         for (x, y) in aligned_shape: 
             cv2.circle(aligned_face, (x, y), 1, (0, 0, 255), -1)
+
 
         ROIs = self.fu.ROI_extraction(aligned_face, aligned_shape)
         gray_val = self.sp.extract_color(ROIs)
 
         self.frame_out = frame
-        self.frame_ROI = aligned_face        
+        self.frame_ROI = aligned_face
+        
+        
         L = len(self.data_buffer)
+        
 
         g = gray_val
-
+        
         if(abs(g-np.mean(self.data_buffer))>10 and L>99): #remove sudden change, if the avg value change is over 10, use the mean of the data_buffer
             g = self.data_buffer[-1]
         
@@ -83,23 +100,23 @@ class Process (object):
         # start calculating after the first 10 frames
         if L == self.buffer_size:
             
-            self.fps = float(L) / (self.times[-1] - self.times[0])#calculate RR using a true fps of processor of the computer, not the fps the camera provide
+            self.fps = float(L) / (self.times[-1] - self.times[0])#calculate HR using a true fps of processor of the computer, not the fps the camera provide
             even_times = np.linspace(self.times[0], self.times[-1], L)
             
-            processed = signal.detrend(processed) #detrend the signal to avoid interference of light change
+            processed = signal.detrend(processed)#detrend the signal to avoid interference of light change
             interpolated = np.interp(even_times, self.times, processed) #interpolation by 1
             interpolated = np.hamming(L) * interpolated#make the signal become more periodic (advoid spectral leakage)
             #norm = (interpolated - np.mean(interpolated))/np.std(interpolated)#normalization
             norm = interpolated/np.linalg.norm(interpolated)
-            raw = np.fft.rfft(norm*30) #do real fft with the normalization multiplied by 10
+            raw = np.fft.rfft(norm*30)#do real fft with the normalization multiplied by 10
             
             self.freqs = float(self.fps) / L * np.arange(L / 2 + 1)
             freqs = 60. * self.freqs
-          
-            
-            self.fft = np.abs(raw)**2 #get amplitude spectrum
         
-            idx = np.where((freqs > 20) & (freqs < 50))#the range of frequency that RR is supposed to be within 
+            
+            self.fft = np.abs(raw)**2#get amplitude spectrum
+        
+            idx = np.where((freqs > 5) & (freqs < 20))#the range of frequency that RR is supposed to be within 
             pruned = self.fft[idx]
             pfreq = freqs[idx]
             
@@ -115,7 +132,7 @@ class Process (object):
             #ifft = np.fft.irfft(raw)
         self.samples = processed # multiply the signal with 5 for easier to see in the plot
         #TODO: find peaks to draw RR-like signal.
-        
+       
         return True
     
     def reset(self):
@@ -146,6 +163,29 @@ class Process (object):
         return y 
     
 
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
         
         
         
